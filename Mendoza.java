f612dfd917aa23173af6814ef5afb46e45a35782
reignm1 <mndzreignmyer.29@gmail.com>
2025-03-06 15:24:46 +0800

    Create Mendoza.java

---
+++ Mendoza.java
@@ -0,0 +1,8 @@
+public class Mendoza {
+    public static void display() {
+        int age = 19;
+        System.out.println("Hello! This is Mendoza contributing to the project.");
+        System.out.println("My current age is: " + 19);
+        System.out.println("Next year, I will be: " + (age + 1));
+    }
+}
